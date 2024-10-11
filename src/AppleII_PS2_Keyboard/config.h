83ae61bda887ff2e34e36852453034e1bfa70b90
Daniel A. Telles <daniel.telles91@yahoo.com>
2024-10-10 22:50:45 -0300

    Update config.h

--- src/AppleII_PS2_Keyboard/config.h
+++ src/AppleII_PS2_Keyboard/config.h
@@ -1,17 +1,17 @@
 #pragma once
 
 // PS/2 interface configuration
-#define PS2_CLOCK_PIN 7
-#define PS2_DATA_PIN 8
+#define PS2_CLOCK_PIN 2
+#define PS2_DATA_PIN 3
 #define PS2_RX_BUFFER_SIZE 64
 
 // AppleII interface configuration
-#define AII_RESET_PIN 0
-#define AII_STROBE_PIN 1
-#define AII_DATA_PIN_0 20
-#define AII_DATA_PIN_1 21
-#define AII_DATA_PIN_2 18
-#define AII_DATA_PIN_3 19
-#define AII_DATA_PIN_4 4
-#define AII_DATA_PIN_5 3
-#define AII_DATA_PIN_6 5
+#define AII_RESET_PIN 4
+#define AII_STROBE_PIN 5
+#define AII_DATA_PIN_0 6
+#define AII_DATA_PIN_1 7
+#define AII_DATA_PIN_2 8
+#define AII_DATA_PIN_3 9
+#define AII_DATA_PIN_4 10
+#define AII_DATA_PIN_5 A0
+#define AII_DATA_PIN_6 A1
